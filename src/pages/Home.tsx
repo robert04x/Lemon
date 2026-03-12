8a864894f7149de91d954b1632d2a84d26f1d0ed
robert04x <rbosman374@gmail.com>
2026-03-12 22:40:33 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -220,6 +220,12 @@ const Home = () => {
                 >
                   Vezi meniul nostru
                 </Link>
+                <Link
+                  to="/menu"
+                  className="inline-block px-6 py-2 sm:px-8 sm:py-3 bg-gradient-to-r from-yellow-400 to-yellow-500 text-white rounded-full font-semibold hover:from-yellow-500 hover:to-yellow-600 transition-all duration-300 shadow-lg hover:shadow-xl transform hover:-translate-y-1 text-sm sm:text-base"
+                >
+                  Pula
+                </Link>
               </motion.div>
             </div>
           </motion.div>
