a9e4209843d3e9c1bc81e4d6e551d7864e64c0fb
robert04x <rbosman374@gmail.com>
2026-03-14 20:12:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Gallery from './pages/Gallery';
 import Menu from './pages/Menu';
 import { Diamond as Lemon } from 'lucide-react';
 
